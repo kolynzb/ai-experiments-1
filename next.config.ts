00a4dc45a629125290e39cbd3d08d7e68b9c48c1
Atuhaire Collins Benda <collinsbenda360@gmail.com>
2024-10-27 14:13:29 +0300

    feat: add experimental packages for webpack

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // (Optional) Export as a standalone site
+  // See https://nextjs.org/docs/pages/api-reference/next-config-js/output#automatically-copying-traced-files
+  output: "standalone", // Feel free to modify/remove this option
+
+  // Indicate that these packages should not be bundled by webpack
+  experimental: {
+    serverComponentsExternalPackages: ["sharp", "onnxruntime-node"],
+  },
 };
 
 export default nextConfig;
